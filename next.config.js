e926ea807be702882214a7ecaa186801762b1f1a
natBizitza <romankevich.law@gmail.com>
2022-01-01 23:23:44 +0100

    feat(next-i18next): add next-i18next config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
+const { i18n } = require('./next-i18next.config');
+
 module.exports = {
   reactStrictMode: true,
-  i18n: {
-    locales: ['en', 'es', 'ru'],
-    defaultLocale: 'en',
-  }
-}
+  i18n,
+};
